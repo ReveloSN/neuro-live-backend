8265198971d19b165899c1c551219fb3a2cd819e
nicolas Revelo <santiago.revelo@campusucc.edu.co>
2026-05-15 03:15:20 -0500

    chore(cleanup): elimina import redundante de TelemetryPayload en BiometricDataDTO

--- src/main/java/com/neurolive/neuro_live_backend/presentation/dto/BiometricDataDTO.java
+++ src/main/java/com/neurolive/neuro_live_backend/presentation/dto/BiometricDataDTO.java
@@ -1,6 +1,5 @@
 package com.neurolive.neuro_live_backend.presentation.dto;
 
-import com.neurolive.neuro_live_backend.presentation.dto.TelemetryPayload;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
